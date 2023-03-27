e549d013dff9b12261f447f32064a1abb2e82a65
VanTai1111 <taiphamvan1111@gmail.com>
2023-03-27 22:08:20 +0700

    create max number

--- src/TestJava/Test.java
+++ src/TestJava/Test.java
@@ -1,8 +1,25 @@
 package TestJava;
-
+import java.util.*;
+import java.util.Scanner;
 public class Test {
+	// nhập ba số bất kì tìm số lớn nhất
 	public static void main(String[] args) {
-		System.out.println("xin chao vantai");
+		Scanner save = new Scanner(System.in);
+		int soThuNhat, soThuHai, soThuBa;
+		System.out.println("Mời nhập số thứ nhất");
+		soThuNhat = save.nextInt();
+		System.out.println("Mời nhập số thứ hai");
+		soThuHai = save.nextInt();
+		System.out.println("Mời nhập số thứ ba");
+		soThuBa = save.nextInt();
+		int soLonNhat = soThuNhat;
+		if (soLonNhat < soThuHai)
+			soLonNhat = soThuHai;
+		if (soLonNhat < soThuBa)
+			soLonNhat = soThuBa;
+		System.out.println("Số lớn nhất trong ba số ta nhập vào là "+soLonNhat);
+		save.close();
 	}
+	
 
 }
